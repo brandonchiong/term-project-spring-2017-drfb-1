7c268fe4bc40dde39156b27434587fe56c3289dd
Brandon Chiong <brandonchiong@gmail.com>
2017-05-08 01:32:59 -0700

    register setup

--- UNO/routes/register.js
+++ UNO/routes/register.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const users = require('../db/Users/index.js');
+
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+router.post('/register', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	var email = req.body.email;
+
+	users.create(username, username, username, email, password)
+	.then(() => {
+		res.render('/login');
+	})
+	.catch(error => {
+		console.log(error)
+	});
 });
 
 module.exports = router;
