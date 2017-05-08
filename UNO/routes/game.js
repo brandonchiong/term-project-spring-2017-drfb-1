d1d0e7aad35e5d134b1592dcbc38d0802fcfabd6
Rainier Hui <rain2288@gmail.com>
2017-05-08 14:53:28 -0700

    added basic game logic

--- UNO/routes/game.js
+++ UNO/routes/game.js
@@ -6,3 +6,37 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+//Game Logic Start
+var cardTurn = false; //clockwise if true, counter clockwise if false
+var currentPlayerTurn = 1;
+
+
+function getNextPlayerTurn(){
+	if (isCardTurnReversed == true){
+
+	}
+}
+
+function isCardTurnReversed(){
+	return cardTurn;
+}
+
+function isCurrentPlayerTurn(){
+	if (true){
+
+	}
+	else alert ("Its not your turn");
+}
+
+function isValidPlay(){
+	//
+	if (){
+		return true;
+	}
+	else return false;
+}
+
+function Update(){
+
+}
