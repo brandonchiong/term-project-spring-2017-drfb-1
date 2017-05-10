44880a217b71bc4d0816a1e4582c80e6d94a265d
Daniel Tri Nguyen <dan.win@gmail.com>
2017-05-10 13:50:56 -0700

    modified game route

--- UNO/routes/game.js
+++ UNO/routes/game.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('game', { title: 'Game' });
-});
+router.get('/', (request, response) => {
+    Cards.all()
+        .then(cards => {
+            response.render('game', { title: 'game', cards })
+            console.log(cards.id);
+        })
+      });
 
 module.exports = router;
