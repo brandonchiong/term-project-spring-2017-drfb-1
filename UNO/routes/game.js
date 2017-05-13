70d799fafc1c9f26329b78449a2001f1659f2460
Brandon Chiong <brandonchiong@gmail.com>
2017-05-13 12:49:25 -0700

    username is logged on console for gamelobby and game

--- UNO/routes/game.js
+++ UNO/routes/game.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+
+	var username = req.user.alias;
+
+	console.log(username + ' joined the game');
+
   res.render('game', { title: 'Game' });
 });
 
