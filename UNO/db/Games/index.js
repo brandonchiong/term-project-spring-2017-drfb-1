d47070873ec4f6192fdefcf619f96d94e019a48c
Felix <ofcfchin@gmail.com>
2017-05-15 14:38:43 -0700

    fixed errors db

--- UNO/db/Games/index.js
+++ UNO/db/Games/index.js
@@ -2,7 +2,7 @@ const db = require( '../connect' )
 
 const ALL = `SELECT * FROM Games`
 const FIND = `SELECT * FROM Games WHERE id=$1`
-const CREATE = `INSERT INTO Games ( player_turn ) VALUES ( true, $1 ) RETURNING id`
+const CREATE = `INSERT INTO Games ( direction, player_turn ) VALUES ( true, $1 ) RETURNING id`
 const DELETE = `DELETE FROM Games WHERE id = $1`
 const GETNUMPLAYERS = 'SELECT COUNT (*) AS num FROM (SELECT DISTINCT user_id FROM GameCards WHERE game_id=$1 AND user_id > 0) AS tmp'
 
