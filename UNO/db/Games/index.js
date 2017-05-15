dbe2d55ecd0c59d86be8ee88f926a143760d8ebf
Felix <ofcfchin@gmail.com>
2017-05-15 14:34:03 -0700

    fixed some db error

--- UNO/db/Games/index.js
+++ UNO/db/Games/index.js
@@ -2,14 +2,14 @@ const db = require( '../connect' )
 
 const ALL = `SELECT * FROM Games`
 const FIND = `SELECT * FROM Games WHERE id=$1`
-const CREATE = `INSERT INTO Games ( top_card_id, direction, player_turn ) VALUES ( -1, true, $2 ) RETURNING id`
+const CREATE = `INSERT INTO Games ( player_turn ) VALUES ( true, $1 ) RETURNING id`
 const DELETE = `DELETE FROM Games WHERE id = $1`
 const GETNUMPLAYERS = 'SELECT COUNT (*) AS num FROM (SELECT DISTINCT user_id FROM GameCards WHERE game_id=$1 AND user_id > 0) AS tmp'
 
 module.exports = {
    all: () => db.any( ALL ),
    find: id => db.oneOrNone( FIND, id ),
-   create: ( direction, player_turn ) => db.oneOrNone( CREATE, [ player_turn ] ),
+   create:  player_turn  => db.oneOrNone( CREATE,  player_turn  ),
    delete: id => db.none( DELETE, id ),
    getNumPlayers: id => db.any( GETNUMPLAYERS, id ),
 }
