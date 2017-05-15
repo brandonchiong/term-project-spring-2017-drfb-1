b97cd77a146f4769931fb5b2b012d827e9ce0815
Felix <ofcfchin@gmail.com>
2017-05-15 14:05:21 -0700

    Added db functions

--- UNO/db/Users/index.js
+++ UNO/db/Users/index.js
@@ -1,15 +1,15 @@
 const db = require('../connect')
 
 const ALL = `SELECT * FROM Users`
-const FIND = `SELECT * FROM Users WHERE id=$1`
-const FIND_BY_ALIAS = `SELECT id, email FROM Users WHERE alias=$1`
-const CREATE = `INSERT INTO Users ( alias, email, pw ) VALUES ( $1, $2, $3 ) RETURNING *`
-const DELETE = `DELETE FROM Users WHERE id = $1`
+const GETUSER = `SELECT * FROM Users WHERE id=$1`
+const GETUSER_BY_ALIAS = `SELECT id, email FROM Users WHERE alias=$1`
+const CREATEUSER = `INSERT INTO Users ( alias, email, pw ) VALUES ( $1, $2, $3 ) RETURNING *`
+const DELETEUSER = `DELETE FROM Users WHERE id = $1`
 
 module.exports = {
    all: () => db.any( ALL ),
-   find: id => db.oneOrNone( FIND, id ),
-   find_by_alias: alias => db.oneOrNone( FIND_BY_ALIAS, alias ),
-   create: ( alias, email, pw ) => db.one( CREATE, [ alias, email, pw ] ),
-   delete: id => db.none( DELETE, id ),
+   getUser: id => db.oneOrNone( GETUSER, id ),
+   getUser_by_alias: alias => db.oneOrNone( GETUSER_BY_ALIAS, alias ),
+   createUser: ( alias, email, pw ) => db.one( CREATEUSER, [ alias, email, pw ] ),
+   deleteUser: id => db.none( DELETEUSER, id ),
 }
