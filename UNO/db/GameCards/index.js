d724059e2422fd07981107ff5e15cc9cc29dcdd4
Felix <ofcfchin@gmail.com>
2017-05-16 20:14:02 -0700

    db fcns added

--- UNO/db/GameCards/index.js
+++ UNO/db/GameCards/index.js
@@ -10,7 +10,18 @@ const GETNUMCARDSBYPLAYER = 'SELECT COUNT (*) AS numCards FROM GameCards WHERE g
 const DRAWCARDBYPLAYERID = 'UPDATE GameCards SET user_id=$1 ' + 
                            'WHERE game_id=$2 AND card_id '+ 
                            'IN (SELECT card_id FROM ' +
-                           'GameCards WHERE game_id =$2 AND user_id=1 AND discarded=false ORDER BY random() LIMIT 1) RETURNING *'
+                           'GameCards WHERE game_id =$2 AND user_id=1 AND discarded=false '+
+                           'ORDER BY random() LIMIT 1) RETURNING *'
+const DRAW2CARDSBYPLAYERID = 'UPDATE GameCards SET user_id=$1 ' + 
+                             'WHERE game_id=$2 AND card_id '+ 
+                             'IN (SELECT card_id FROM ' +
+                             'GameCards WHERE game_id =$2 AND user_id=1 AND discarded=false '+
+                             'ORDER BY random() LIMIT 2) RETURNING *'
+const DRAW4CARDSBYPLAYERID = 'UPDATE GameCards SET user_id=$1 ' + 
+                             'WHERE game_id=$2 AND card_id '+ 
+                             'IN (SELECT card_id FROM ' +
+                             'GameCards WHERE game_id =$2 AND user_id=1 AND discarded=false '+
+                             'ORDER BY random() LIMIT 4) RETURNING *'
 const PLAYCARD = 'UPDATE GameCards SET discarded=true, discarded_at=$4, user_id=1 ' + 
                  'WHERE game_id=$1 AND user_id=$2 AND card_id=$3 RETURNING card_id'
 const RESET = 'UPDATE GameCards SET discarded=false, discarded_at=\'2017-05-17 00:00:00\', user_id=1 WHERE game_id=$1 AND discarded=true AND user_id=1'
@@ -134,16 +145,33 @@ const NEWDECK = 'INSERT INTO GameCards (game_id, card_id, user_id, discarded_at,
 module.exports = {
    all: () => db.any( ALL ),
    getCardsByPlayer: ( game_id, user_id ) => db.any( GETCARDSBYPLAYER, [ game_id, user_id ] ),
+   
+   /*Called when a new game is starting to set all game cards into the deck belonging to the default user=1.
+     Call drawTopCard after this.*/
    newDeck: (game_id) => db.any( NEWDECK, game_id ),
    deletePlayer: ( game_id, user_id ) => db.one( DELETEPLAYER, [ game_id, user_id ] ),
    deleteGame: game_id => db.one( DELETEGAME, game_id),
    cardToPlayer: ( game_id, card_id, user_id ) => db.one( CARDTOPLAYER, [ game_id, card_id, user_id] ),
    getNumCardsInDeck: (game_id) => db.any(GETNUMCARDSINDECK, game_id),
    getNumCardsByPlayer: (game_id, user_id) => db.any(GETNUMCARDSBYPLAYER, [game_id, user_id]),
+   
+   //Player draws from deck
    drawCardByPlayerId: (user_id, game_id) => db.one(DRAWCARDBYPLAYERID,[ user_id, game_id ] ),
-   playCard: (game_id, user_id, card_id, time) => db.one(PLAYCARD, [ game_id, user_id, card_id, time ]) ,//---> Games.setTopCard(result.card_id)
-   reset: game_id => db.any(RESET, game_id),
-   drawTopCard :(game_id, time)=> db.one(DRAWTOPCARD, [ game_id, time ]) //---> Games.setTopCard(result.card_id)
+   
+   //Player plays a draw 2 card on next player| user_id == the player receiving.
+   draw2CardsByPlayerId: (user_id, game_id) => db.one(DRAW2CARDSBYPLAYERID,[ user_id, game_id ] ),
+   
+   //Player plays a draw 4 card on the next player| user_id == the player receiving.
+   draw4CardsByPlayerId: (user_id, game_id) => db.one(DRAW4CARDSBYPLAYERID,[ user_id, game_id ] ),
+  
+   //Call this after playCard ---> Games.setTopCard(result.card_id)
+   playCard: (game_id, user_id, card_id, time) => db.one(PLAYCARD, [ game_id, user_id, card_id, time ]) ,
+   
+   //Recommend a reset when number of cards in deck == 4 or some number close to but higher. 
+   reset: game_id => db.any(RESET, game_id), 
+   
+   //Call this after newDeck() ---> Games.setTopCard(result.card_id)
+   drawTopCard :(game_id, time)=> db.one(DRAWTOPCARD, [ game_id, time ]) 
                 
        
 }
